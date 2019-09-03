d4b3d0ad0e88e6a3efe141161aaef24ac605e457
cwindy <54516640+cwindy@users.noreply.github.com>
2019-09-02 20:18:09 -0400

    Update A1Jedi.java

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -1,5 +1,7 @@
 package a1;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class A1Jedi {
@@ -8,7 +10,63 @@ public class A1Jedi {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+
+		int count = scan.nextInt();
+		
+		/*
+		 * create catalog lists
+		 * */
+		String[] catalogName = new String[count];
+		double[] catalogPrice = new double[count];
+		for(int i = 0; i<count; i++) {
+			catalogName[i] = scan.next();
+			catalogPrice[i] = scan.nextDouble();
+		}
+		/* 
+		 * customer info 
+		 * */
+		int custNum = scan.nextInt();
+		
+		int itemNum;
+		String itemName;
+		int itemCount;
+		
+		int[] catalogCount = new int[count];
+		int[] custCount = new int[count];
+		for(int i = 0; i < count; i++) {
+			catalogCount[i]=0;
+			custCount[i]=0;
+		}
+		
+		/* scan in specific customer info*/
+		for(int i = 0; i < custNum; i++) {
+			
+			scan.next(); scan.next();
+			
+			itemNum = scan.nextInt();
+			
+			for(int j = 0; j<itemNum; j++) {
+				
+				itemCount = scan.nextInt();
+				itemName = scan.next();
+				for(int k = 0; k<count; k++) {
+					
+					if(itemName.equals(catalogName[k])) {
+						custCount[k] ++;
+						catalogCount[k] += itemCount;
+					}
+				}
+				
+			}
+		}
+		
+		for (int i = 0; i < count; i++) {
+			if (custCount[i] ==0)
+				System.out.println("No customers bought " + catalogName[i]);
+			else
+				System.out.println(custCount[i] + " customers bought " + catalogCount[i] + " " + catalogName[i]);
+			
+		}
 		
 	}
 }
