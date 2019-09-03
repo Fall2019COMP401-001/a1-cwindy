0a4c7033f5ccc6890a5aabe619179f07a4cf2ade
cwindy <54516640+cwindy@users.noreply.github.com>
2019-09-02 20:18:36 -0400

    Update A1Novice.java

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,6 +9,40 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		int count = scan.nextInt();
 		
+		
+		String[] fName = new String[count];
+		String[] lName = new String[count];
+		double[] total = new double[count];
+		
+		int itemTot;
+		int itemNum;
+		String itemName;
+		double itemCost;
+		
+		for(int i=0; i<count; i++) {
+			
+			fName[i] = scan.next();
+			lName[i] = scan.next();
+			total[i] = 0;
+			itemTot = scan.nextInt();
+			
+			for(int j = 0; j<itemTot; j++) {
+				itemNum = scan.nextInt();
+				itemName = scan.next();
+				itemCost = scan.nextDouble();
+				total[i] += itemNum * itemCost;
+				
+			}
+			
+		}
+		
+		for(int i=0; i<count; i++) {
+			
+			System.out.println(fName[i].substring(0,1) + ". " +
+					lName[i] + ": " + String.format("%.2f", total[i]));
+			
+		}
 	}
 }
